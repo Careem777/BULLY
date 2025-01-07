a1f59aabfc8d1e2f1012378cde1b452705d14a83
Careem <careemdev7@gmail.com>
2025-01-07 21:16:18 +0530

    conversion added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=Ramesh # no space before and after equal
+PERSION2=Suresh
+
+echo " $PERSION1: ho $PERSION2"
+echo " $PERSION2 : hello $PERSION1,"
+echo "$PERSION1 : how are you doing?"
+echo "$PERSION2: i am good. how are you?"
