18288457fdfacde9c78a03176e26d1220eef8f8c
Careem <careemdev7@gmail.com>
2025-01-07 21:35:08 +0530

    conversion added

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=$1
+PERSION2=$2
+
+echo " $PERSION1: ho $PERSION2"
+echo " $PERSION2 : hello $PERSION1,"
+echo "$PERSION1 : how are you doing?"
+echo "$PERSION2: i am good. how are you?"
